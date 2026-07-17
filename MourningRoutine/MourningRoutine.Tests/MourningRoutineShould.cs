bcda05974bef94ec4810c30274beb26249119862
TrikerLh <corchero100@gmail.com>
2026-07-17 15:02:02 +0200

    green: eturnDoExerciseBetween6To7

---
+++ MourningRoutine/MourningRoutine.Tests/MourningRoutineShould.cs
@@ -0,0 +1,21 @@
+﻿using AwesomeAssertions;
+
+namespace MourningRoutine.Tests;
+
+public class MourningRoutineShould
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void ReturnDoExerciseBetween6To7()
+    {
+        var mourningRoutine = new MyMourningRoutine();
+
+        var response = mourningRoutine.WhatShouldIDoNow();
+
+        response.Should().Be("From 06:00 to 06:59 - Do exercise");
+    }
+}
---
+++ MourningRoutine/MourningRoutine/MyMourningRoutine.cs
@@ -0,0 +1,9 @@
+namespace MourningRoutine;
+
+public class MyMourningRoutine
+{
+    public string WhatShouldIDoNow()
+    {
+        return "From 06:00 to 06:59 - Do exercise";
+    }
+}
