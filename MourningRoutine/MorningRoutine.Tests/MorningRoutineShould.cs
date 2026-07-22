478a0a2986ef3e135818bb0dd7943629d91555ba
Aaronhdez <aaronhdezalv@hotmail.com>
2026-07-22 11:28:58 +0100

    red: ReturnStudyBetween7To8

--- MourningRoutine/MorningRoutine.Tests/MorningRoutineShould.cs
+++ MourningRoutine/MorningRoutine.Tests/MorningRoutineShould.cs
@@ -18,4 +18,14 @@ public class MorningRoutineShould
 
         response.Should().Be("From 06:00 to 06:59 - Do exercise");
     }
+
+    [Test]
+    public void ReturnStudyBetween7To8()
+    {
+        var mourningRoutine = new MyMorningRoutine();
+        
+        var response = mourningRoutine.WhatShouldIDoNow();
+        
+        response.Should().Be("From 07:00 to 07:59 - Read and study");
+    }
 }
