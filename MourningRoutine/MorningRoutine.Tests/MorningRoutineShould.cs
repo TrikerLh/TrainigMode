bd3cf25184fd5b09905df980b215aa5d7b07ab76
Aaronhdez <aaronhdezalv@hotmail.com>
2026-07-22 11:40:42 +0100

    green: ReturnStudyBetween7To8

--- MourningRoutine/MorningRoutine.Tests/MorningRoutineShould.cs
+++ MourningRoutine/MorningRoutine.Tests/MorningRoutineShould.cs
@@ -1,18 +1,23 @@
 ﻿using AwesomeAssertions;
+using NSubstitute;
 
 namespace MourningRoutine.Tests;
 
 public class MorningRoutineShould
 {
+    private IClock _mockClock;
+
     [SetUp]
     public void Setup()
     {
+        _mockClock = Substitute.For<IClock>();
     }
 
     [Test]
     public void ReturnDoExerciseBetween6To7()
     {
-        var mourningRoutine = new MyMorningRoutine();
+        var mourningRoutine = new MyMorningRoutine(_mockClock);
+        _mockClock.Now().Returns(new DateTime(2026, 07, 22, 6, 1, 0));
 
         var response = mourningRoutine.WhatShouldIDoNow();
 
@@ -22,7 +27,8 @@ public class MorningRoutineShould
     [Test]
     public void ReturnStudyBetween7To8()
     {
-        var mourningRoutine = new MyMorningRoutine();
+        var mourningRoutine = new MyMorningRoutine(_mockClock);
+        _mockClock.Now().Returns(new DateTime(2026, 07, 22, 7, 1, 0));
         
         var response = mourningRoutine.WhatShouldIDoNow();
         
--- MourningRoutine/MorningRoutine/MyMorningRoutine.cs
+++ MourningRoutine/MorningRoutine/MyMorningRoutine.cs
@@ -2,8 +2,26 @@ namespace MourningRoutine;
 
 public class MyMorningRoutine
 {
+    private IClock _internalClock;
+
+    public MyMorningRoutine(IClock internalClock)
+    {
+        _internalClock = internalClock;
+    }
+
     public string WhatShouldIDoNow()
     {
-        return "From 06:00 to 06:59 - Do exercise";
+        var now = _internalClock.Now().TimeOfDay;
+        if (now < new TimeSpan(0, 7, 0, 0)
+            && now >= new TimeSpan(0, 6, 0, 0))
+        {
+            return "From 06:00 to 06:59 - Do exercise";
+        }
+        return "From 07:00 to 07:59 - Read and study";
     }
+}
+
+public interface IClock
+{
+    DateTime Now();
 }
