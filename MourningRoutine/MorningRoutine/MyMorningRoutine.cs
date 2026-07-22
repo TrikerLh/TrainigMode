7b468791d6d9d3bd8fec034d2163c9d1afe55ab0
TrikerLh <corchero100@gmail.com>
2026-07-22 12:57:14 +0200

    refactor: extracted method

--- MourningRoutine/MorningRoutine/MyMorningRoutine.cs
+++ MourningRoutine/MorningRoutine/MyMorningRoutine.cs
@@ -5,16 +5,22 @@ public class MyMorningRoutine(IClock internalClock)
     public string WhatShouldIDoNow()
     {
         var now = internalClock.Now().TimeOfDay;
-        if (now < new TimeSpan(0, 7, 0, 0)
-            && now >= new TimeSpan(0, 6, 0, 0))
+        if (Between6And7(now))
         {
             return "From 06:00 to 06:59 - Do exercise";
         }
-        if (now < new TimeSpan(0, 8, 0, 0)
-            && now >= new TimeSpan(0, 7, 0, 0))
+        if (Between7And8(now))
         {
             return "From 07:00 to 07:59 - Read and study";
         }
         return "From 08:00 to 08:59 - Have breakfast";
     }
+
+    private static bool Between7And8(TimeSpan now) =>
+        now < new TimeSpan(0, 8, 0, 0)
+        && now >= new TimeSpan(0, 7, 0, 0);
+
+    private static bool Between6And7(TimeSpan now) =>
+        now < new TimeSpan(0, 7, 0, 0)
+        && now >= new TimeSpan(0, 6, 0, 0);
 }
